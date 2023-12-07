d60151cc21a741266fcfeb5287c02a888cf2eba3
Zicco-codes <xeccokavie@gmail.com>
2023-12-06 16:41:51 -0800

    update

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    result_matrix = []
+
+    for row in matrix:
+
+        squared_row = [num ** 2 for num in row]
+
+        result_matrix.append(squared_row)
+
+    return result_matrix
