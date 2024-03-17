4bf1a1e71bab14c8ece5dc4988a7d117f0953333
Zicco-codes <xeccokavie@gmail.com>
2024-03-17 16:04:38 -0700

    update complete

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from enum import unique
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class State"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
